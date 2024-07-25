b37bb09f9660b172398e373281864849e60d41b2
kjh <1rlawogns1@naver.com>
2024-07-25 13:48:48 +0900

    tlqkf

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ const App = () => {
   const [cart, setCart] = useState([]);
 
   const products = [
-    { id: 1, name: '브랜드A', description: '편안하고 착용감이 좋은 신발', price: '35,000', image: '/image1.png' },
-    { id: 2, name: '브랜드A', description: '힙한 컬러가 매력적인 신발', price: '25,000', image: '/image2.png' },
-    { id: 3, name: '브랜드B', description: '편안하고 착용감이 좋은 신발', price: '35,000', image: '/image3.png' },
-    { id: 4, name: '브랜드B', description: '힙한 컬러가 매력적인 신발', price: '35,000', image: '/image4.png' },
-    { id: 5, name: '브랜드C', description: '편안하고 착용감이 좋은 신발', price: '35,000', image: '/image5.png' },
-    { id: 6, name: '브랜드C', description: '힙한 컬러가 매력적인 신발', price: '35,000', image: '/image6.png' }
+    { id: 1, name: '브랜드A', description: '편안하고 착용감이 좋은 신발', price: '35,000', image: '../image1.png' },
+    { id: 2, name: '브랜드A', description: '힙한 컬러가 매력적인 신발', price: '25,000', image: '../image2.png' },
+    { id: 3, name: '브랜드B', description: '편안하고 착용감이 좋은 신발', price: '35,000', image: '../image3.png' },
+    { id: 4, name: '브랜드B', description: '힙한 컬러가 매력적인 신발', price: '35,000', image: '../image4.png' },
+    { id: 5, name: '브랜드C', description: '편안하고 착용감이 좋은 신발', price: '35,000', image: '../image5.png' },
+    { id: 6, name: '브랜드C', description: '힙한 컬러가 매력적인 신발', price: '35,000', image: '../image6.png' }
   ];
 
   const toggleCart = (product, isAdding) => {
